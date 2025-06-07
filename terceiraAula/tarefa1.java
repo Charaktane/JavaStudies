cc5178b101c16e9dd4fc8d9932db0d7c08c015aa
S. Silva Domingos <suyannegomes01@gmail.com>
2025-06-07 17:12:56 -0300

    Create tarefa1.java

---
+++ terceiraAula/tarefa1.java
@@ -0,0 +1,8 @@
+public class tarefa1 {
+	public static void main(String[] args) {
+		String descricao = "Par de meia";
+		float custo = 3.30f;
+		float venda = 5.40f;
+		float lucro = venda - custo;
+		System.out.printf("O lucro da venda é: " + lucro);
+}}
