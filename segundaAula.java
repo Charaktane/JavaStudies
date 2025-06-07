f8b93b9c9c34bbe04a405cbaaee12a536136446b
S. Silva Domingos <suyannegomes01@gmail.com>
2025-06-07 16:37:40 -0300

    Add files via upload

---
+++ segundaAula.java
@@ -0,0 +1,28 @@
+//Para atualizar é javac segundaAula.java
+//Para rodar é java segundaAula
+public class segundaAula {
+   public static void main(String[] args){
+     /*//Variaveis do tipo inteiro
+     int idade = 40;
+     // Variaveis com casas decimais
+     float divida = 1200.67f; // O f transforma em valor 
+     String nome = "Joao";
+     Boolean casado = false; // Verdadeiro ou falso
+
+     System.out.println(nome);
+     System.out.println(idade);
+     System.out.println(divida);
+   System.out.println(casado); */
+	/*String nome = "Pedro";
+	int idade = 20;
+	String email = "pedrocabuloso@gmail.com";
+	System.out.println("Nome: " + nome);
+	System.out.println("Idade: " + idade);
+	System.out.println("Email: " + email);*/
+	String produto = "Arroz";
+	System.out.println("Produto: " + produto);
+	float preco = 10.40f;
+    System.out.printf("Preço: %.2f n", preco);
+   }
+   
+}
---
+++ tarefa1.java
@@ -0,0 +1,13 @@
+//codigo 105
+//produto caneca
+//preço: 11,50
+public class tarefa1 {
+   public static void main(String[] args){
+	String produto = "caneca";
+	float preco = 11.50f;
+	int codigo = 105;
+	System.out.println("Código: " + codigo);
+	System.out.println("Produto: " + produto);
+	System.out.println("Preço: " + preco);
+	}
+}
