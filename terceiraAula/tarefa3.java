f53b8dfa4b07f58e6c581022379b0a34e51ea081
S. Silva Domingos <suyannegomes01@gmail.com>
2025-06-07 17:13:39 -0300

    Create tarefa3.java

---
+++ terceiraAula/tarefa3.java
@@ -0,0 +1,8 @@
+public class tarefa3 {
+	public static void main(String[] args) {
+		int idade = 15;
+		if (idade > 16){
+			System.out.println("Você não tem 16 anos!!");}
+		else {
+		System.out.println("Você tem 16 anos.");}
+}}
