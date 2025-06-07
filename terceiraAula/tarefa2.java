40489d6b4a9737f4280fb21769139d307df06ec2
S. Silva Domingos <suyannegomes01@gmail.com>
2025-06-07 17:13:22 -0300

    Create tarefa2.java

---
+++ terceiraAula/tarefa2.java
@@ -0,0 +1,9 @@
+public class tarefa2 {
+	public static void main(String[] args) {
+		String descricao = "Leite";
+		int quantidade = 5;
+		float preco = 2.65f;
+		float total = quantidade * preco;
+		System.out.println("O total ficou de: " + total);
+	}
+}
